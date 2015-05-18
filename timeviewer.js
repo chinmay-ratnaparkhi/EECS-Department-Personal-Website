9e3827c9ec821a1a5cb50f81f3dea8ad842b2347
c732r120 <chinmay.ratnaparkhi@gmail.com>
2015-05-17 21:49:09 -0500

    Time
    Added current time viewer and updated location

---
+++ timeviewer.js
@@ -0,0 +1,12 @@
+/*
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+document.getElementById("current_time").innerHTML= datetime.toLocaleString();
+*/
+document.getElementById("current_time").innerHTML= "ji";
